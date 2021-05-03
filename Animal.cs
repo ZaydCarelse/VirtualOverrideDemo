c00f2f020ba341b987d55d16943470b926621d2e
Zayd Carelse <zayd.carelse@gmail.com>
2021-05-03 20:39:44 +0200

    Moved solution to the proper directory

---
+++ Animal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace VirtualOverrideDemo
+{
+    public class Animal
+    {
+        //Animal properties
+        private string Name { get; set; }
+        private int Age { get; set; }
+        private bool IsHungry { get; set; }
+        
+        //Animal Constructor
+        public Animal(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            IsHungry = true;
+        }
+        
+        //The noise the animal makes
+        public virtual void MakeSound()
+        {
+            
+        }
+        
+        //The animal will eat when it is hungry
+        public virtual void Eat()
+        {
+            if (IsHungry)
+            {
+                Console.WriteLine($"{Name} is eating.");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} is not hungry.");
+            }
+        }
+        
+        //The animal will play
+        public virtual void Play()
+        {
+            Console.WriteLine($"{Name} is playing.");
+        }
+    }
+}
